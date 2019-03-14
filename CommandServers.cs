446594a6915e0fd7fcfa3498c15615d4cf5132f4
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2019-03-14 18:41:37 +0000

    Added new "Permission" configuration.
    This adds the new Permission option to the configuration!

--- CommandServers.cs
+++ CommandServers.cs
@@ -1,4 +1,4 @@
-﻿using Rocket.API;
+using Rocket.API;
 using Rocket.Unturned.Player;
 using Rocket.Unturned.Chat;
 using System.Collections.Generic;
@@ -27,20 +27,27 @@ namespace ServerSwitcher
 
             if (Command.Length > 0) { UnturnedChat.Say(uPlayer, Syntax, Color.red); return; }
 
+            int Count = 0;
             if (ServerSwitcher.Instance.Configuration.Instance.Servers.Count > 0)
-            {
-                int Count = 0;
+            {               
                 foreach (Server Server in ServerSwitcher.Instance.Configuration.Instance.Servers)
                 {
-                    Count++;
-                    UnturnedChat.Say(uPlayer, $"[{Count}/{ServerSwitcher.Instance.Configuration.Instance.Servers.Count}] {Server.Name} [{Server.IP}]");
+                    if (uPlayer.HasPermission("serverswitcher.server.*") || uPlayer.HasPermission($"serverswitcher.server.{Server.Permission}"))
+                    {
+                        Count++;
+                        UnturnedChat.Say(uPlayer, $"[{Count}/{ServerSwitcher.Instance.Configuration.Instance.Servers.Count}] {Server.Name} [{Server.IP}]", Color.yellow);
+                    }     
                 }
             } else
             {
                 UnturnedChat.Say(uPlayer, "The are no Servers to view!", Color.red);
                 return;
             }
-            
+
+            if (Count == 0)
+            {
+                UnturnedChat.Say(uPlayer, "There are no Servers to display! Check you have the correct Permissions!", Color.red);
+            }            
         }
 
     }
