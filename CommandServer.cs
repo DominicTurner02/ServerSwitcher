b9dee8ed5f0f085ef4d8c06b4da589cca75d8e7b
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2019-04-12 01:18:04 +0100

    Update CommandServer.cs

--- CommandServer.cs
+++ CommandServer.cs
@@ -6,48 +6,51 @@ using UnityEngine;
 
 namespace ServerSwitcher
 {
-    public class CommandServers : IRocketCommand
+    public class CommandServer : IRocketCommand
     {
         public AllowedCaller AllowedCaller => AllowedCaller.Player;
 
-        public string Name => "servers";
+        public string Name => "server";
 
-        public string Help => "Lists the available servers";
+        public string Help => "Moves you to a different Server";
 
-        public string Syntax => "/servers";
+        public string Syntax => "/server [Server Name]";
 
         public List<string> Aliases => new List<string>();
 
-        public List<string> Permissions => new List<string>() { "serverswitcher.servers" };
+        public List<string> Permissions => new List<string>() { "serverswitcher.server" };
+
 
         public void Execute(IRocketPlayer rCaller, string[] Command)
         {
             UnturnedPlayer uPlayer = (UnturnedPlayer)rCaller;
 
-            if (Command.Length > 0) { UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_incorrect_syntax_server"), Color.red); return; }
+            if (Command.Length > 1 || Command.Length == 0) { UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_incorrect_syntax_server"), Color.red); return; }
 
-            int Count = 0;
             if (ServerSwitcher.Instance.Configuration.Instance.Servers.Count > 0)
-            {               
+            {
                 foreach (Server Server in ServerSwitcher.Instance.Configuration.Instance.Servers)
                 {
-                    if (uPlayer.HasPermission("serverswitcher.server.*") || uPlayer.HasPermission($"serverswitcher.server.{Server.Permission}"))
+                    if (Server.Name.ToLower() == Command[0].ToLower())
                     {
-                        Count++;
-                        UnturnedChat.Say(uPlayer, $"[{Count}/{ServerSwitcher.Instance.Configuration.Instance.Servers.Count}] {Server.Name} [{Server.IP}]", Color.yellow);
-                    }    
+                        if (uPlayer.HasPermission("serverswitcher.server.*") || uPlayer.HasPermission($"serverswitcher.server.{Server.Permission}"))
+                        {
+                            ServerSwitcher.Instance.StartSwitch(Server, uPlayer);
+                            return;
+                        } else
+                        {
+                            UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_no_permission"), Color.red);
+                            return;
+                        }                        
+                    }
                 }
+                UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_server_doesnt_exist", Command[0]), Color.red);
+                return;
             } else
             {
-                UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_no_view_servers"), Color.red);
+                UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_no_view_servers", Command[0]), Color.red);
                 return;
             }
-
-            if (Count == 0)
-            {
-                UnturnedChat.Say(uPlayer, ServerSwitcher.Instance.Translate("direct_no_display_servers"), Color.red);
-            }            
         }
-
     }
 }
