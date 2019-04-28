a5f332d962716f64cc287bcc89a12b551f35fd9d
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2019-04-28 23:19:57 +0100

    Added support for RocketRegions!

--- ConfigurationServerSwitcher.cs
+++ ConfigurationServerSwitcher.cs
@@ -6,14 +6,16 @@ namespace ServerSwitcher
 {
     public class ConfigurationServerSwitcher : IRocketPluginConfiguration
     {
+        public bool RocketRegionsSupport;
         [XmlArrayItem(ElementName = "Server")]
         public List<Server> Servers;
 
         public void LoadDefaults()
         {
+            RocketRegionsSupport = false;
             Servers = new List<Server>()
             {
-                new Server() { Name = "Server1", IP = "127.0.0.1", Port = 27015, Password = "password", Permission = "server1", Delay = 5f }
+                new Server() { Name = "Server1", IP = "127.0.0.1", Port = 27015, Password = "password", Permission = "server1", Delay = 5f, CanBeUsedInRocketRegion = true, IgnoreDelayInRocketRegion = false}
             };
         }
     }
@@ -28,6 +30,8 @@ namespace ServerSwitcher
         public string Password;
         public string Permission;
         public float Delay;
+        public bool CanBeUsedInRocketRegion;
+        public bool IgnoreDelayInRocketRegion;
 
     }
 }
