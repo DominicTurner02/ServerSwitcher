43e909e0d10c7ea647b3fd4392dfa714fc808b1c
MrKwabs <40438275+MrKwabs@users.noreply.github.com>
2019-04-29 22:10:23 +0100

    Adding fix to bug.
    This bug could possibility make it so you can't join the original Server if you are moved to a different Server whilst entering a new region.

---
+++ SwitchPlayer.cs
@@ -0,0 +1,15 @@
+﻿using Rocket.Unturned.Player;
+
+namespace ServerSwitcher
+{
+    public class SwitchPlayer : UnturnedPlayerComponent
+    {
+        public long JoinUnix { get; set; }
+
+        public SwitchPlayer()
+        {
+            JoinUnix = 0;
+        }
+
+    }
+}
